64ba6a647064060f19d6d44064c6f5f88e37a94f
leeceo97 <leeceo97@naver.com>
2023-09-30 10:40:53 +0900

    (fix)logging

--- config/settings/deploy.py
+++ config/settings/deploy.py
@@ -34,62 +34,35 @@ CORS_ALLOWED_ORIGIN_REGEXES = [
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
-    "filters": {
-        "require_debug_false": {
-            "()": "django.utils.log.RequireDebugFalse",
-        },
-        "require_debug_true": {
-            "()": "django.utils.log.RequireDebugTrue",
-        },
-    },
+    "root": {"level": "DEBUG", "handlers": ["file"]},
     "formatters": {
-        "django.server": {
-            "()": "django.utils.log.ServerFormatter",
-            "format": "[{server_time}] {message}",
-            "style": "{",
+        "verbose": {
+            "format": (
+                "%(asctime)s %(levelname)s %(name)s %(message)s"
+                " [PID:%(process)d:%(threadName)s]"
+            )
         },
-        "standard": {"format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s"},
+        "simple": {"format": "%(levelname)s %(message)s"},
     },
+    "filters": {"require_debug_false": {"()": "django.utils.log.RequireDebugFalse"}},
     "handlers": {
-        "console": {
-            "level": "INFO",
-            "filters": ["require_debug_true"],
-            "class": "logging.StreamHandler",
-        },
-        "django.server": {
-            "level": "INFO",
-            "class": "logging.StreamHandler",
-            "formatter": "django.server",
-        },
-        "mail_admins": {
-            "level": "ERROR",
-            "filters": ["require_debug_false"],
-            "class": "django.utils.log.AdminEmailHandler",
-        },
         "file": {
             "level": "INFO",
-            "encoding": "utf-8",
-            "filters": ["require_debug_false"],
             "class": "logging.handlers.RotatingFileHandler",
             "filename": BASE_DIR / "logs/mysite.log",
-            "maxBytes": 1024 * 1024 * 5,  # 5 MB
+            "maxBytes": 10 * 1024 * 1024,  # 10MB
             "backupCount": 5,
-            "formatter": "standard",
-        },
+            "formatter": "verbose",
+        }
     },
     "loggers": {
-        "django": {
-            "handlers": ["console", "mail_admins", "file"],
+        "django": {"handlers": ["file"], "level": "INFO", "propagate": True},
+        "django.server": {"handlers": ["file"], "level": "INFO", "propagate": True},
+        "django.db.backends": {
+            "handlers": ["file"],
             "level": "INFO",
+            "propagate": True,
         },
-        "django.server": {
-            "handlers": ["django.server", "file"],
-            "level": "INFO",
-            "propagate": False,
-        },
-        "accounts": {
-            "handlers": ["console", "file"],
-            "level": "INFO",
-        },
+        "unicommerce": {"handlers": ["file"], "level": "INFO", "propagate": True},
     },
 }
