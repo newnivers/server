325895a9719a0d83263894cb85d13d31dbe73de9
mac <mac@macui-MacBookPro.local>
2024-01-01 22:52:17 +0900

    (add)comment admin

--- apps/arts/admin.py
+++ apps/arts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
-from apps.arts.models import Art, ArtSchedule, Ticket
+from apps.arts.models import Art, ArtSchedule, Ticket, Comment
 
-admin.site.register([Art, ArtSchedule, Ticket])
+admin.site.register([Art, ArtSchedule, Ticket, Comment])
