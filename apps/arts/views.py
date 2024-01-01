1a4c94a19e9d91e2cce9d2b830dd6e476b7a1a3c
mac <mac@macui-MacBookPro.local>
2024-01-01 22:57:36 +0900

    (fix)comment swaggers

--- apps/arts/views.py
+++ apps/arts/views.py
@@ -170,4 +170,47 @@ class CommentViewSet(mixins.CreateModelMixin,
                      BaseViewSet,):
     serializer_class = CommentSerializer
     queryset = Comment.objects.all()
-    
+
+    @swagger_auto_schema(
+        operation_summary="작품 후기 생성 API",
+        manual_parameters=[
+            auth_parameter,
+        ],
+    )
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return self.get_response("후기 생성에 성공했습니다.", serializer.data, status.HTTP_201_CREATED)
+
+    @swagger_auto_schema(
+        operation_summary="작품 후기 수정 API",
+        manual_parameters=[
+            auth_parameter,
+        ],
+    )
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return self.get_response("후기 수정에 성공했습니다.", serializer.data, status.HTTP_200_OK)
+
+    @swagger_auto_schema(
+        operation_summary="작품 후기 삭제 API",
+        manual_parameters=[
+            auth_parameter,
+        ],
+    )
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return self.get_response("후기 삭제에 성공했습니다.", status.HTTP_204_NO_CONTENT)
